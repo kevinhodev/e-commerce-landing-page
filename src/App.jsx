1fb365c712c831d660d665f1b376bfda23846b40
Kevin Ramos Dos Santos De Oliveira <kevram52@outlook.com>
2022-06-01 21:23:58 -0300

    Arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./components/Container/Container";
+import Header from "./components/Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <Header />
+      </Container>
+    </div>
+  );
+};
+
+export default App;
